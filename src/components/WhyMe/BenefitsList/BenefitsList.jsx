d9375fd15726126d9fe4d7b6f6a33ff256340ab2
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-08-18 11:46:44 +0200

    updated data fetch and style in why me

--- src/components/WhyMe/BenefitsList/BenefitsList.jsx
+++ src/components/WhyMe/BenefitsList/BenefitsList.jsx
@@ -1,67 +1,16 @@
 import css from "./BenefitsList.module.css";
-import { LiaPenSolid } from "react-icons/lia";
-import { PiLightbulbFilament } from "react-icons/pi";
-import { FaRegStar } from "react-icons/fa6";
-import { BsLightningCharge } from "react-icons/bs";
-import { FiTool } from "react-icons/fi";
+// import { LiaPenSolid } from "react-icons/lia";
+// import { PiLightbulbFilament } from "react-icons/pi";
+// import { FaRegStar } from "react-icons/fa6";
+// import { BsLightningCharge } from "react-icons/bs";
+// import { FiTool } from "react-icons/fi";
+import data from "../data/why_me_data.json";
+import ListItemCard from "../ListItemCard/ListItemCard";
 
 const BenefitsList = () => {
   return (
     <ul className={css.list}>
-      <li className={css.listItem}>
-        <div className={css.container}>
-          <LiaPenSolid className={css.icon} />
-          <h3 className={css.title}>agency</h3>
-          <p className={css.desc}>
-            Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum
-            sed libero sed at sed nisl sodales phasellus et.
-          </p>
-        </div>
-      </li>
-      <li className={css.listItem}>
-        <div className={css.container}>
-          <PiLightbulbFilament className={css.icon} />
-
-          <h3 className={css.title}>choose idea</h3>
-          <p className={css.desc}>
-            Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum
-            sed libero sed at sed nisl sodales phasellus et.
-          </p>
-        </div>
-      </li>
-      <li className={css.listItem}>
-        <div className={css.container}>
-          <FaRegStar className={css.icon} />
-
-          <h3 className={css.title}>satisfaction</h3>
-          <p className={css.desc}>
-            Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum
-            sed libero sed at sed nisl sodales phasellus et.
-          </p>
-        </div>
-      </li>
-      <li className={css.listItem}>
-        <div className={css.container}>
-          <FiTool className={css.icon} />
-
-          <h3 className={css.title}>quality</h3>
-          <p className={css.desc}>
-            Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum
-            sed libero sed at sed nisl sodales phasellus et.
-          </p>
-        </div>
-      </li>
-      <li className={css.listItem}>
-        <div className={css.container}>
-          <BsLightningCharge className={css.icon} />
-
-          <h3 className={css.title}>creativity</h3>
-          <p className={css.desc}>
-            Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum
-            sed libero sed at sed nisl sodales phasellus et.
-          </p>
-        </div>
-      </li>
+      <ListItemCard data={data} />{" "}
     </ul>
   );
 };
--- src/components/WhyMe/WhyTitleAndDesc/WhyTitleAndDesc.jsx
+++ src/components/WhyMe/WhyTitleAndDesc/WhyTitleAndDesc.jsx
@@ -1,13 +1,11 @@
 import css from "./WhyTitleAndDesc.module.css";
+import data from "../data/why_me_data.json";
 
 const WhyTitleAndDesc = () => {
   return (
     <div className={css.container}>
-      <h2 className={css.title}>why me</h2>
-      <p className={css.desc}>
-        Tincidunt sagittis mollis nec suspendisse sed felis massa urna nec. Urna
-        quisque blandit turpis ultrices arcu ut sed.
-      </p>
+      <h2 className={css.title}>{data.main_title}</h2>
+      <p className={css.desc}>{data.main_desc}</p>
     </div>
   );
 };
